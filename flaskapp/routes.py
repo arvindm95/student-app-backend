6b8f60904775636d0e6d967bc4890075e6c23665
Aswin Kumar <aswin.kumar@coda.global>
2018-03-29 14:00:41 +0530

    Added student api

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,16 +1,44 @@
 from flask import Flask, render_template, jsonify
-from flaskext.mysql import MySQL  # import mysql
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.inspection import inspect
 import json
 
 app = Flask(__name__)
 
-# mysql config
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'student_app_db'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/student_app_db'
+db = SQLAlchemy(app)
+
+
+class Serializer(object):
+
+    def serialize(self):
+        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+    @staticmethod
+    def serialize_list(l):
+        return [m.serialize() for m in l]
+
+
+class Student(db.Model, Serializer):
+    student_id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(45))
+    student_email = db.Column(db.String(45))
+    student_username = db.Column(db.String(45))
+    student_password = db.Column(db.String(45))
+    student_department = db.Column(db.String(45))
+
+    def __init__(self, student_id, student_name, student_email, student_username, student_password, student_department):
+        self.student_id = student_id
+        self.student_name = student_name
+        self.student_email = student_email
+        self.student_username = student_username
+        self.student_password = student_password
+        self.student_department = student_department
+
+    def serialize(self):
+        d = Serializer.serialize(self)
+        del d['student_password']
+        return d
 
 
 @app.route('/')
@@ -23,21 +51,24 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/getUser')
-def getUser():
-    # mysql connection
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * from student")
-    data = cursor.fetchall()
-    # print data
-    return jsonify(data)
+@app.route('/student/all')
+def getAllStudents():
+    students = Student.query.all()
+    response = Student.serialize_list(students)
+    return jsonify(response)
+
+
+@app.route('/student/<student_id>')
+def getStudent(student_id):
+    student = Student.query.get(student_id)
+    response = student.serialize()
+    return jsonify(response)
 
 
 @app.route('/status')
 def getStatus():
     #response.headers['Content-Type'] = 'application/json'
-    return jsonify({'tasks': 'hello'})
+    return jsonify({'status': 'service working'})
 
 
 if __name__ == '__main__':
